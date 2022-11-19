d9fbdfff4e9f62b6c96f5aafed26f83384d949e6
deniz hrnjic <73642343+denizzzzzzz@users.noreply.github.com>
2022-11-19 21:29:48 +0100

    Routes/Eerste vraag.
    Ik heb react-router-dom toegevoegd aan mijn project en een route voor de homepagina en eerste vraag aangemaakt.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import Question1 from './components/Question1';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    <Home/>
-    </div>
+
+      <div className="App">
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Vraag1" element={<Question1/>}/>
+  </Routes>
+         
+
+      </div>
+
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,14 @@ import VrworldLogo from '../assets/HomeLogo.png';
 const Home = () => {
   return (
     <div class="flex flex-col justify-center items-center p-4 md:p-24 ">
-    <h1 class= "HomeTitle text-4xl text-white pb-10 pt-4 md:text-7xl lg:text-8xl lg:pt-8">Deel uw ervaring.</h1>
+    <h1 class= "HomeTitle text-5xl text-white pb-10 pt-4 md:text-7xl lg:text-8xl lg:pt-8">Deel uw ervaring.</h1>
       <div class="container bg-[#343232] rounded-lg lg:w-max">
         <div class="text-container bg-[#767474] rounded-lg text-white md:text-2xl md:max-w lg:text-2xl lg:pb-2 lg:max-w ">
           <p>Welkom, met de knop hieronder kunt u door middel van een paar vragen</p><p>uw ervaring over VR-World met ons delen.</p> 
           <p>Want uw mening telt!</p>
         </div>
         <button class="hover:bg-[#85550E]  relative bg-black rounded-lg text-white p-6 mt-4 md:text-2xl lg:text-xl lg:pb-8 lg:p-2">
-          Klik hier om uw ervaring te delen
+        <a href='/Vraag1'>Klik hier om uw ervaring te delen</a>
         <div class="absolute bottom-0 right-0  bg-[#d9d9d962] rounded-lg text-[#ffffffc1] ">
             <p class="text-sm">Geschatte tijd 3 min</p>
         </div>
