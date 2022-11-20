c3536f3dea66deace480fd1b27ba5443b73d4563
deniz hrnjic <73642343+denizzzzzzz@users.noreply.github.com>
2022-11-20 12:14:05 +0100

    Layout vraag 1
    Ik heb de layout voor vraag 1 gemaakt, weet nog niet of het er uiteindelijk ook zo uit gaat zien.

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
           <p>Welkom, met de knop hieronder kunt u door middel van een paar vragen</p><p>uw ervaring over VR-World met ons delen.</p> 
           <p>Want uw mening telt!</p>
         </div>
-        <button class="hover:bg-[#85550E]  relative bg-black rounded-lg text-white p-6 mt-4 md:text-2xl lg:text-xl lg:pb-8 lg:p-2">
+        <button class="hover:bg-[#85550E]  relative bg-black rounded-lg text-white p-6 mt-4 md:text-2xl lg:text-xl lg:pb-8 lg:p-2 " >
         <a href='/Vraag1'>Klik hier om uw ervaring te delen</a>
         <div class="absolute bottom-0 right-0  bg-[#d9d9d962] rounded-lg text-[#ffffffc1] ">
             <p class="text-sm">Geschatte tijd 3 min</p>
--- src/components/Question1.js
+++ src/components/Question1.js
@@ -1,8 +1,42 @@
 import React from 'react'
-
+import PicQ1 from '../assets/FotoVraag1.jpg';
 const Question1 = () => {
   return (
-    <div>Question1</div>
+    <div class="flex justify-center items-center p-4 md:p-24 ">
+      <div class="container bg-[#343232] rounded-lg w-1/2">
+        <div class="text-container text-4xl  bg-[#767474] rounded-lg text-white pb-24 pt-4 ">
+          <p>Hoe was uw ervaring met VR-World Breda over het algemeen?</p>
+        </div>
+
+        <div class="Form-Container flex flex-row pt-20 pb-8 gap-60 pl-10">
+        <div class="text-white text-3xl">
+          <label>Goed</label>
+        <input type="radio" value="Goed" name="Vraag1" class="w-5 h-5 m-2"/>
+        </div>
+        <div class="text-white text-3xl">
+        <label>Slecht</label>
+        <input type="radio" value="Goed" name="Vraag1" class="w-5 h-5 m-2"/>
+        </div>
+        </div>
+
+        <div class="Form-Container flex flex-col lg:flex lg:flex-row gap-72  ">
+        <div class="text-white text-3xl">
+        <label>Vreselijk</label>
+        <input type="radio" value="Goed" name="Vraag1" class="w-5 h-5 m-2"/>
+        </div>
+        <div class="text-black text-2xl">
+        <input type="text-area" placeholder="Iets anders/Toevoeging" name="Vraag1" class="pb-12 rounded-lg " />
+        </div>
+       
+      
+        </div>
+        <div class="flex flex-col justify-center items-center"><img src={PicQ1} class="h-64 w-168  m-8 border-4 "></img>
+        </div>
+        
+</div>
+</div>
+
+
   )
 }
 
