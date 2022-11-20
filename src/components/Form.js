8040a0f6d333e6632172c1221eb5f4f408e781f1
deniz hrnjic <73642343+denizzzzzzz@users.noreply.github.com>
2022-11-20 15:03:58 +0100

    Vragen aangemaakt.
    Ik heb useState geimport en vervolgens op regel 5 deze een key, value gegeven en deze op 0 gezet zodat de useState bij vraag 1 begint. daarna heb ik een array gemaakt met alle vragen en deze geimplementeerd op regel 21 vervolgens heb ik de buttons next en prev code gegeven voor het loopen door de array wanneer er op geklikt word.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Form() {
+
+const [Question, setQuestion] = useState(0);
+
+const QuestionTitles = 
+        [
+        "Hoe was uw ervaring met VR-World Breda over het algemeen?", 
+        "Wat vond u van de games bij VR-World Breda?",
+        "Zou u VR-World Breda nog een keer bezoeken?",
+        "Wat kon er beter bij VR-World Breda?",
+        "Tot slot, welk cijfer zou u VR-World Breda geven?"
+        ]
   return (
     <div className='form'>
         <div className='progressbar'></div>
 
                 <div className='form-container'>
-                <div className='header'></div>
+                <div className='header text-white text-xl'>
+                    <h1>{QuestionTitles[Question]}</h1>
+                </div>
                 <div className='body'></div>
                 <div className='footer'>
-                    <button className='text-white'>Prev</button>
-                    <button className='text-white'>Next</button>
+                <button className='text-white'
+                disabled={Question == 0}
+                onClick={()=>{
+                    setQuestion((currPage)=> currPage -1);
+                }}>
+                    Prev
+                    </button>
+                <button className='text-white'
+                    disabled={Question == QuestionTitles.length -1}
+                    onClick={()=>{
+                        setQuestion((currPage)=> currPage +1);
+                    }}>
+                    Next
+                    </button>
             </div>
         </div>
     </div>
